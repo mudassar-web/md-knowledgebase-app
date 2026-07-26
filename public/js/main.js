1077029b729c4146858ee13f7da7d6622fe008e5
mudassar-web <mudassar.ansari@gmail.com>
2026-07-26 16:13:23 +0530

    Add vercel domain for delete operation

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 async function callDeleteRoute(id) {
     try {
-        const response = await fetch(`http://localhost:5000/articles/${id}`, {
+        const response = await fetch(`https://md-knowledgebase-app.vercel.app/articles/${id}`, {
             method: 'DELETE'
         });
 
